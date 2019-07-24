562bd12f7df4e63c82be188edb0a9ecd452ca6bf
Bob Jeltes <bobjeltes@hotmail.com>
2019-07-24 21:49:58 +0200

    V0.1.4
    Code
    - Rework of how items are added to and removed from the inventory

--- Context-GameJam-Group10-UnityProj/Assets/Scripts/Inventory/Inventory.cs
+++ Context-GameJam-Group10-UnityProj/Assets/Scripts/Inventory/Inventory.cs
@@ -19,26 +19,28 @@ public class Inventory : MonoBehaviour {
 
     public void AddToInventory(GameObject item, int slot) {
         GameObject currentSlot = InventorySlots[slot];
+        InventorySlotScript currentSlotScript = currentSlot.GetComponent<InventorySlotScript>();
         Image currentSlotImage = currentSlot.GetComponent<InventorySlotScript>().ItemImage;
         Button currentSlotRemoveButton = currentSlot.GetComponent<InventorySlotScript>().ItemRemoveButton;
         Sprite itemSprite = item.GetComponent<SpriteRenderer>().sprite;
 
-        currentSlotImage.enabled = true;
-        currentSlotImage.sprite = item.GetComponent<SpriteRenderer>().sprite;
+        currentSlotScript.HeldObject = item;
+        currentSlotScript.ItemImage.enabled = true;
+        currentSlotScript.ItemImage.sprite = item.GetComponent<SpriteRenderer>().sprite;
 
         currentSlotRemoveButton.interactable = true;
     }
 
     public void RemoveFromInventory(int slot) {
-        GameObject item = Items[slot];
+        InventorySlotScript currentSlotScript = InventorySlots[slot].GetComponent<InventorySlotScript>();
+        GameObject item = currentSlotScript.HeldObject;
+        
         item.SetActive(true);
         item.transform.position = Player.position;
         Items[slot] = null;
 
-        GameObject currentSlot = InventorySlots[slot];
-
-        currentSlot.GetComponent<InventorySlotScript>().ItemImage.enabled = false;
-        currentSlot.GetComponent<InventorySlotScript>().ItemRemoveButton.interactable = false;
+        currentSlotScript.ItemImage.enabled = false;
+        currentSlotScript.GetComponent<InventorySlotScript>().ItemRemoveButton.interactable = false;
 
         Debug.Log("Dropping item");
     }
